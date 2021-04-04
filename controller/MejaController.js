0d21d9d83fed0fe09c59690b1b44f3f9acf6c72f
apriadi4425 <apriadi441994@gmail.com>
2021-04-04 09:55:06 +0800

    membuat controller meja

---
+++ controller/MejaController.js
@@ -0,0 +1,16 @@
+const models = require('../models')
+
+exports.PanggilMejaPtsp = (req, res) => {
+    models.DataMeja.findAll({raw : true, where : {tv : 1}})
+        .then(result => {
+            res.send({
+                status : 'sukses',
+                data : result
+            })
+        }).catch(err => {
+            res.status(500).send({
+                status : 'error',
+                data : err
+            })
+        })
+}
